5705967e2dd8cde07c85e714177d9cefee62f4f8
Ayuminmin <masamichiayu@gmail.cim>
2019-03-07 14:15:18 +0800

    remake spell

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      flash[:success] = "sucess sign up"
+      flash[:success] = "Successfully sign up"
       redirect_to root_url
     else
       render 'new'
