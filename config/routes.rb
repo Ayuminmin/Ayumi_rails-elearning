d78d6c2bc7cfbe0aec9c10bc13581082b2e4cfa2
Ayuminmin <masamichiayu@gmail.cim>
2019-03-07 14:27:12 +0800

    create_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
