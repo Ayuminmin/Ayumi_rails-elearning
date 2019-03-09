446d17f805cde06ea9d6cdf57805260d87a67ce7
Ayuminmin <masamichiayu@gmail.cim>
2019-03-09 17:25:42 +0800

    created helper_method(user)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,23 @@
 module UsersHelper
-    def gravatar_for(user,options={size:80})
-        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        image_size = options[:size]
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{image_size}"
-        image_tag(gravatar_url, alt: user.name)
-    end
+  def gravatar_for(user,options={size:80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    image_size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{image_size}"
+    image_tag(gravatar_url, alt: user.name)
+  end 
+  def helper_method(user)
+    # @user = User.find(params[:id])
+    if user.admin?
+      button_to 'Remove as Admin', admin_user_url(user), method: :patch, class: "btn"
+    else
+      link_to 'Set as Admin', admin_user_url(user), method: :put, class: "btn"
+    end 
+  end
 end
+
+
+
+
+
+
+
