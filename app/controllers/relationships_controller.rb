fe18fad8e0ba7d4b686a212d866227a75f5d0e94
Ayuminmin <masamichiayu@gmail.cim>
2019-03-28 17:19:01 +0800

    design

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,13 @@ class RelationshipsController < ApplicationController
     current_user.follow(user)
     relationship = Relationship.find_by(follower_id: current_user.id, followed_id: user.id)
     relationship.create_activity(user_id: current_user.id)
-    redirect_to user
+    redirect_to request.referer
   end
   
   def destroy
     user = Relationship.find(params[:id]).followed      
     current_user.unfollow(user)
-    redirect_to user
+    redirect_to request.referer
   end
+
 end
