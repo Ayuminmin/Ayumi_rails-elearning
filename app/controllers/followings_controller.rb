6773ef16213dd63e321440ff52d1728239bbc057
Ayuminmin <masamichiayu@gmail.cim>
2019-03-29 15:47:40 +0800

    fix categories_index

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,6 +1,6 @@
 class FollowingsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @follow_users = @user.following.paginate(page: params[:page], per_page: 10)
+    @follow_users = @user.following.paginate(page: params[:page], per_page: 8)
   end
 end
