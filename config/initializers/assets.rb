fdf4d50907756813367d03aa08d8ef8ded4a46de
Ayuminmin <44863815+Ayuminmin@users.noreply.github.com>
2019-03-29 16:12:13 +0800

    Fix (#17)
    * fix categories_index
    
    * fix dezign

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w( static_pages.css )
+Rails.application.config.assets.precompile += %w( categories.css )
+Rails.application.config.assets.precompile += %w( users.css )
