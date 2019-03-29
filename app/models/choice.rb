0f7a5cfd4aa0dc9e213c6b9eb7316c86d709dc25
Ayuminmin <masamichiayu@gmail.cim>
2019-03-29 17:08:14 +0800

    result.count

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,6 +2,7 @@ class Choice < ApplicationRecord
   belongs_to :word
   has_many :answers, dependent: :destroy
   has_many :lessons, through: :answers
+  has_many :categories, through: :word
 
   validates :content, presence: :true,
               length: {maximum: 50}    
